d70b231876f4a387c34bb6c1f352e6e20d5e30bb
zinaw2017 <zinaw2017@yahoo.com>
2017-12-03 17:11:59 -0500

    final commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Chicken Sandwich', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Burger King Whopper', false);
---
+++ server.js
@@ -0,0 +1,33 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+// Import MySQL connection.
+// var connection = require("../config/connection.js");
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+// Listening at port
+app.listen(port, function() {
+	console.log("Listening on " + port);
+});
