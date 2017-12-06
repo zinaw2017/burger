87130c5d676db6000d09e1cce1e3bd2a60d8fdae
Zenahibzu <30580746+zinaw2017@users.noreply.github.com>
2017-12-06 10:07:16 -0500

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,3 +2,7 @@
 INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Chicken Sandwich', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Big Mac', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Burger King Whopper', false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Cheese', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Whopper', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beef Double Cheese Burger', false);
